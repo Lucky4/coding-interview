f0475f3964fa12de30b08d072f63a490258a1a15
lucky4 <591398706@qq.com>
2018-06-05 12:21:32 +0800

    update

---
+++ 4.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+
+class FindGreatestSumOfSubArray:
+    def method1(self, array):
+        max_sum = array[0]
+        array_length = len(array)
+        for i in range(0, array_length):
+            curr_sum = 0
+            for j in range(i, array_length):
+                curr_sum += array[j]
+                if curr_sum > max_sum:
+                    max_sum = curr_sum
+        return max_sum
+
+    def method2(self, array):
+        '''
+        对于一个数A，若是A的左边累计数非负，那么加上A能使得值不小于A，认为累计值对
+        整体和是有贡献的。如果前几项累计值负数，则认为有害于总和，total记录当前值。
+        '''
+        curr_sum = 0
+        max_sum = array[0]
+        for i in array:
+            if curr_sum <= 0:
+                curr_sum = i
+            else:
+                curr_sum += i
+            if curr_sum > max_sum:
+                max_sum = curr_sum
+        return max_sum
+    
+    def method3(self, array):
+        '''
+        动态规划，以array[i]为末尾元素的子数组的和的最大值，子数组的元素的相对位置不变
+        F（i）= max（F（i-1）+array[i] ， array[i]）
+        '''
+        max_sum = curr_max = array[0]
+        array_length = len(array)
+        for i in array[1:]:
+            curr_max = max(curr_sum+i, i)
+            max_sum = max(curr_max, max_sum)
+        return max_sum
--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 |1|[二进制中1的个数](https://www.nowcoder.com/practice/8ee967e43c2c4ec193b040ea7fbb10b8?tpId=13&tqId=11164&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking) | [Python](./1.py)
 |2|[不用加减乘除做加法](https://www.nowcoder.com/practice/59ac416b4b944300b617d4f7f111b215?tpId=13&tqId=11201&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking) | [C++](./2.cpp)
 |3|[求1+2+...+n](https://www.nowcoder.com/practice/7a0da8fc483247ff8800059e12d7caf1?tpId=13&tqId=11200&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking) | [Python](./3.py)
+|4|[最大子数组](https://www.nowcoder.com/practice/459bd355da1549fa8a49e350bf3df484?tpId=13&tqId=11183&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking) | [Python](./4.py)
