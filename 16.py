2aa2e2c58218295dcc4aa071013e9b68e9416f49
lucky4 <591398706@qq.com>
2018-06-14 03:39:00 +0800

    update

--- 16.py
+++ 16.py
@@ -17,3 +17,29 @@ class Solution:
             self.dict1[char] = self.dict1[char]+1
         else:
             self.dict1[char] = 1
+
+
+# 另一种写法，主要熟悉一下队列的用法。
+#from collections import deque
+#
+#
+#class Solution:
+#    # 返回对应char
+#    def __init__(self):
+#        self.q = deque()
+#        self.s = {}
+#        
+#    def FirstAppearingOnce(self):
+#        while len(self.q) and self.s[self.q[0]] == 2:
+#            self.q.popleft()
+#            
+#        if len(self.q) == 0:
+#            return '#'
+#        
+#        return self.q[0]
+#        
+#    def Insert(self, char):
+#        if not self.s.has_key(char):
+#            self.s[char] = 1
+#            self.q.append(char) else:
+#            self.s[char] += 1
