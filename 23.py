9b300d49f5dd785d3c6a5253f91eec993088106e
lucky4 <591398706@qq.com>
2018-06-19 04:48:03 -0400

    update

---
+++ 23.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    # 和我的思路想符的写法，时间复杂度为O(mn)，如果使用Python语言编写不能通过。
+    def LastRemaining_Solution(self, n, m):
+        if n < 1 or m < 1:
+            return -1
+
+        flags = [None] * n
+        count = n
+        step = 0
+        i = -1
+
+        while count > 0:
+            i += 1
+            if i >= n: i = 0
+            if flags[i] == 1: continue
+            step += 1
+            if step == m:
+                step = 0
+                count -= 1
+                flags[i] = 1
+
+        return i
+
+    def LastRemaining_Solution2(self, n, m):
+        if n < 1 or m < 1:
+            return -1
+        last = 0
+        for i in range(2, n + 1):
+            last = (last + m) % i
+        return last
+
+
+# 本题实际上就是约瑟夫环问题,记住公式:
+#                  0         , n = 1
+# f(n, m)= [f(n-1, m)+m] % n , n > 1
