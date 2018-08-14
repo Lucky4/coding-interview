85fca78fe21a1741fef782e8a9338abb6a5d7d0a
Lucky4 <591398706@qq.com>
2018-08-14 20:11:54 +0800

    Update 35.py

--- 35.py
+++ 35.py
@@ -3,7 +3,7 @@ class Solution:
     # 集合的子集个数为2的集合长度次幂
     def PowerSet(self, ss):
 	size = len(ss)
-        pow_ss_size = pow(2, size)
+	pow_ss_size = pow(2, size)
 	res = []
 
 	for count in range(pow_ss_size):
