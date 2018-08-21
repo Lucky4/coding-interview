f7a644b0758f3a4b0c1b17eecd8404c5119102d2
lucky4 <591398706@qq.com>
2018-08-22 07:47:43 +0800

    update

--- 34.py
+++ 34.py
@@ -15,8 +15,7 @@ class Solution:
     def Helper(self, ss_list, i, res_list):
         if i == len(ss_list) - 1:
             res = ''.join(ss_list)
-            if res not in res_list:
-                res_list.append(res)
+            res_list.append(res)
 
         for j in range(i, len(ss_list)):
             self.swap(ss_list, i, j)
@@ -29,9 +28,11 @@ class Solution:
         res_list[j] = tmp
 
 
-# 疑问1：swap(self, res_list, i, j)函数如果写成swap(self, i, j)这种形式，为什么没有起到交换作用？
-# 答：因为swap()函数中i，j都是形参，函数内为参数赋予新值不会改变外部任何变量，其中的运行机制相当于，swap()函数获得主函数实参的拷贝。
+# 疑问：swap(self, res_list, i, j)函数如果写成swap(self, i, j)这种形式，为什么没有起到交换作用？
+#   答：swap(self, i, j)函数被调用后，实参通过赋值传递给形参，传递的是对象（引用类型），
+#       我们的i，j是局部变量，指向不可变类型，对i，j赋值对实参没有影响；对i，j操作时对可变参数会产生影响
 # 
+#
 # 解法2：使用Python内置的itertools.permutations排列方法，要记住itertools中常用的方法。
 # import itertools
 # class Solution:
@@ -39,5 +40,6 @@ class Solution:
 #         if not ss:
 #             return []
 #         return sorted(list(set(map(''.join, itertools.permutations(ss)))))
+#
 #
 # 解法3：该问题还可使用减治的思想。后续有时间补充。（ps：估计是不会补充了。。）
