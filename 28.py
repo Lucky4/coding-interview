b6496c7631bc588c9d858354b4a5f3f42269c920
lucky4 <591398706@qq.com>
2018-08-24 03:47:18 +0800

    update

--- 28.py
+++ 28.py
@@ -1,31 +1,36 @@
 # -*- coding:utf-8 -*-
 class Solution:
-    # 前面是偶数,后面是奇数,奇数与偶数交换,中间可能有多个偶数,顺序将其右移.
     def reOrderArray(self, array):
-        for i in range(0, len(array) - 2):
-            if not self.isEven(array[i]):
+        for i in range(len(array)):
+            for j in range(len(array)-i-1):
+                if self.isEven(array[j]) and not self.isEven(array[j+1]):
+                    tmp = array[j]
+                    array[j] = array[j+1]
+                    array[j+1] = tmp
+        return array
+
+    # 不要求顺序
+    def reOrderArray2(self, array):
+        start = 0
+        end = len(array)-1
+
+        while start < end:
+            if not self.isEven(array[start]):
+                start += 1
                 continue
 
-            j = i + 1
-            while j !=len(array) and self.isEven(array[j]):
-                j += 1
-
-            if j < len(array):
-                tmp = array[j]
-                while j != i:
-                    array[j] = array[j - 1]
-                    j -= 1
-                array[j] = tmp
-            else:
-                break
+            if self.isEven(array[end]):
+                end -= 1
+                continue
+            
+            tmp = array[start]
+            array[start] = array[end]
+            array[end] = tmp
 
         return array
 
     def isEven(self, number):
-        return not number & 1
-
-#这个题要重做
+        return number & 1 == 0
 
 
 # 1. 学习判断偶数的高效写法.
-# 2. 注意循环中变量索引是否超出范围.
