9681a841a8bb81a42fbab1db5d993d7067e6896b
lucky4 <591398706@qq.com>
2018-09-03 08:25:00 +0800

    update

--- 35.py
+++ 35.py
@@ -1,20 +1,18 @@
 # -*- coding:utf-8 -*-
 class Solution:
-    # 集合的子集个数为2的集合长度次幂
     def PowerSet(self, ss):
-	size = len(ss)
-	pow_ss_size = pow(2, size)
-	res = []
-
-	for count in range(pow_ss_size):
-	    tmp = []
-	    for j in range(size):
-                if count & (1 << j):
+        str_length = len(ss)
+        num_of_set = pow(2, str_length) # 集合的子集个数为2的集合长度次幂
+        
+        res = []
+        for i in range(num_of_set):
+            tmp = []
+            for j in range(str_length):
+                if i & (1 << j):
                     tmp.append(ss[j])
             res.append(''.join(tmp))
 
-	return res
+        return res
 
 
-s = Solution()
-print s.PowerSet('abc')
+print Solution().PowerSet('abc')
