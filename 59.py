376efe7ce2f742f40c488500b0fa50eac5ab1a6c
Lucky4 <591398706@qq.com>
2018-09-03 15:48:37 +0800

    Update 59.py

--- 59.py
+++ 59.py
@@ -3,32 +3,32 @@ class Solution(object):
         """
         题目思路：
 
-        如果 nums[mid] 和 target 相等，那么返回 True。
-        如果 nums[mid] 比 nums[l] 大或者 nums[mid] 比 nums[h] 大，那么说明左边的递增序列比右边的递增序列长。
-        如果 nums[mid] 比 nums[l] 小或者 nums[mid] 比 nums[h] 小，那么说明右边的递增序列比左边的递增序列长。
+        如果 nums[m] 和 target 相等，那么返回 True。
+        如果 nums[m] 比 nums[l] 大或者 nums[mid] 比 nums[h] 大，那么说明左边的递增序列比右边的递增序列长。
+        如果 nums[m] 比 nums[l] 小或者 nums[mid] 比 nums[h] 小，那么说明右边的递增序列比左边的递增序列长。
         """
         l = 0
         h = len(nums) - 1
-        mid = -1
+        m = -1
         
         while l <= h:
-            mid = (l + h) / 2
+            m = (l + h) / 2
             
-            if nums[mid] == target:
+            if nums[m] == target:
                 return True
             
-            if nums[mid] > nums[l] or nums[mid] > nums[h]:
-                if target >= nums[l] and target < nums[mid]:
-                    h = mid - 1
+            if nums[m] > nums[l] or nums[m] > nums[h]:
+                if target >= nums[l] and target < nums[m]:
+                    h = m - 1
                 else:
-                    l = mid + 1
+                    l = m + 1
                 continue
                     
-            if nums[mid] < nums[l] or nums[mid] < nums[h]:
-                if target <= nums[h] and target > nums[mid]:
-                    l = mid + 1
+            if nums[m] < nums[l] or nums[m] < nums[h]:
+                if target <= nums[h] and target > nums[m]:
+                    l = m + 1
                 else:
-                    h = mid - 1
+                    h = m - 1
                 continue
                     
             h -= 1
