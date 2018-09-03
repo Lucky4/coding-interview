1c8b08c0726e516625e5e46b8df973c1538f9932
lucky4 <591398706@qq.com>
2018-09-03 10:57:38 +0800

    update

--- 42.py
+++ 42.py
@@ -12,7 +12,7 @@ class LCS:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         print dp
         return dp[n][m]
-
+        
 
 # 注意，不要使用列表乘法定义二维数组，如[[0] * (m+1)] * (n+1)的方式定义的。因为乘法只是简单的创建了Array引用，其中某个改变会引起其他改变。
-参考：https://github.com/CyC2018/Interview-Notebook/blob/master/notes/Leetcode%20%E9%A2%98%E8%A7%A3.md#%E6%9C%80%E9%95%BF%E5%85%AC%E5%85%B1%E5%AD%90%E5%BA%8F%E5%88%97
+# 参考：https://github.com/CyC2018/Interview-Notebook/blob/master/notes/Leetcode%20%E9%A2%98%E8%A7%A3.md#%E6%9C%80%E9%95%BF%E5%85%AC%E5%85%B1%E5%AD%90%E5%BA%8F%E5%88%97
