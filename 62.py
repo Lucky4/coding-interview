15fced75bc83e9d227b2f79afdab0e55342d4ad3
lucky4 <591398706@qq.com>
2018-09-07 04:00:13 +0800

    update

---
+++ 62.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def FindPath(self, root, expectNumber):
+        res = []
+        path = []
+        self.helper(root, expectNumber, res, path)
+        return res
+
+    def helper(self, root, expectNumber, res, path):
+        if root != None:
+            path.append(root.val)
+            
+            if root.left == None and root.right == None:
+                if reduce(lambda x, y : x + y, path) == expectNumber:
+                    res.append(path[:])
+
+            if root.left != None:
+                self.helper(root.left, expectNumber, res, path)
+            if root.right != None:
+                self.helper(root.right, expectNumber, res, path)
+
+            path.pop()
+
+
+T = TreeNode(10)
+T.left = TreeNode(5)
+T.right = TreeNode(12)
+T.left.left = TreeNode(4)
+T.left.right = TreeNode(7)
+print Solution().FindPath(T, 22)
