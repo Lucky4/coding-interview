ef18dca56ac94ac568eee89da05a290e8fa9cd75
Lucky4 <591398706@qq.com>
2018-09-11 21:15:11 +0800

    Update 47.py

--- 47.py
+++ 47.py
@@ -3,48 +3,30 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-class Solution:
-    # 返回ListNode
+
+
+class Solution(object):
+    # 非递归写法
     def ReverseList(self, pHead):
-        p = pHead
-        if p == None:
-            return None
-        
-        q = pHead.next
-        if q == None:
-            return p
-        
-        r = q.next
-        if r == None:
-            q.next = p
-            p.next = None
-            return q
-        
-        q.next = p
-        p.next = None
-        p = q
-        q = r
-        r = r.next
-        while r != None:
-            q.next = p
-            p = q
-            q = r
-            r = r.next
-            
-        q.next = p
-        return q
+        if not pHead or not pHead.next:
+            return pHead
 
+        last = None
+        while pHead:
+            tmp = pHead
+            pHead = pHead.next
+            tmp.next = last
+            last = tmp
+        return last
+    
 
-# 我的解决思路最先处理了第一个节点，稍有些复杂。更高效的方式应该从None开始构造一个逆置的链表。
-# class Solution:
-#     def ReverseList(self, pHead):
-#         if not pHead or not pHead.next:
-#             return pHead
-
-#         last = None
-#         while pHead:
-#             tmp = pHead
-#             pHead = pHead.next
-#             tmp.next = last
-#             last = tmp
-#         return last
+class Solution2(object):
+    # 递归写法
+    def ReverseList(self, pHead):
+        if not pHead or not pHead.next:
+            return pHead
+        
+        new_head = self.ReverseList(pHead.next)
+        pHead.next.next = head
+        pHead.next = None
+        return new_head
