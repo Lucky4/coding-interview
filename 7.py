88a53f799291041bf870a2da04ec7c12964d40b5
Lucky4 <591398706@qq.com>
2018-09-24 14:49:50 +0800

    Update 7.py

--- 7.py
+++ 7.py
@@ -5,20 +5,9 @@ from heapq import *
 class GetLeastNumbersSolution(object):
     def method1(self, tinput, k):
         '''
-	简单排序，复杂度O(n*log(n))。
-	'''
-        if k > len(tinput) or k == 0:
-            return []
-
-        tinput.sort()
-	return tinput[0:k]
-
-    def method2(self, tinput, k):
-        '''
 	冒牌排序k次，复杂度O(n*k)。
 	'''
-	if k > len(tinput) or k == 0:
-            return []
+	if k > len(tinput) or k == 0: return []
 
         for i in range(0, k):
             for j in range(0, len(tinput)-i-1):
@@ -28,11 +17,8 @@ class GetLeastNumbersSolution(object):
 		    tinput[j] = tinput[j] ^ tinput[j+1]
 
 	return tinput[len(tinput):-(k+1):-1]
-	#res = tinput[-k:]
-	#res.reverse()
-	#return res
 
-    def method3(self, tinput, k):
+    def method2(self, tinput, k):
         '''
 	构建堆， 调整k次，复杂度O(k*log(n))
 	'''
