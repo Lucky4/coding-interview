28eded90dabe263aa8c301965a225bee1e8ec8a1
Lucky4 <591398706@qq.com>
2018-09-24 14:42:29 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,16 +1,5 @@
 # -*- coding: utf-8 -*-
 class FindGreatestSumOfSubArray(object):
-    def method1(self, array):
-        max_sum = array[0]
-        array_length = len(array)
-        for i in range(0, array_length):
-            curr_sum = 0
-            for j in range(i, array_length):
-                curr_sum += array[j]
-                if curr_sum > max_sum:
-                    max_sum = curr_sum
-        return max_sum
-
     def method2(self, array):
         '''
         联机算法，对于一个数A，若是A的左边累计数非负，那么加上A能使得值不小于A，认为累计值对
@@ -33,7 +22,7 @@ class FindGreatestSumOfSubArray(object):
         F（i）= max（F（i-1）+array[i] ， array[i]）
         '''
         max_sum = curr_max = array[0]
-        for i in array[1:]:
-            curr_max = max(curr_max+i, i)
+        for i in range(1, len(array)):
+            curr_max = max(curr_max+i, array[i])
             max_sum = max(curr_max, max_sum)
         return max_sum
