843caf0ca460b063636100e7b6b42c97600341a0
Lucky4 <591398706@qq.com>
2018-09-24 15:59:46 +0800

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,18 +1,10 @@
 # -*- coding:utf-8 -*-
 class Solution:
-    # 这里要特别注意~找到任意重复的一个值并赋值到duplication[0]
-    # 函数返回True/False
     def duplicate(self, numbers, duplication):
         tmp = [False] * len(numbers)
-        
         for i in xrange(0, len(numbers)):
             if tmp[numbers[i]] == True:
                 duplication[0] = numbers[i]
                 return True
-            
             tmp[numbers[i]] = True
-            
         return False
-
-
-# ps: 经我自己证明，使用布尔类型比使用数字更省空间。
