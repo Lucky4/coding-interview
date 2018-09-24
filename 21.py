61813c61b0b3c5fb43270f15e70e5809715c2046
Lucky4 <591398706@qq.com>
2018-09-24 19:40:45 +0800

    Update 21.py

--- 21.py
+++ 21.py
@@ -6,6 +6,7 @@ class Solution:
     def Power(self, base, exponent):
         if base == 0:
 	    return False
+
         if exponent == 0:
 	    return 1
 
@@ -27,21 +28,24 @@ class Solution:
 	return res
 
     def Power2(self, base, exponent):
-        is_minus = False
         if base == 0:
             return False
+
+        if exponent == 0:
+            return 1
+
+        is_minus = False
         if exponent < 0:
             is_minus = True
             exponent = abs(exponent)
-        if exponent == 0:
-            return 1
+
         if exponent == 1: # 这部是处理奇数偶数的关键，运行到这里就返回了，没有继续执行下面的操作。
 	    return base
 
         res = self.Power3(base, exponent >> 1)
-	res *= res # 从这部往下的操作只有在第一次调用时执行了一次
+	
+	res *= res
 	if exponent & 1 == 1:
-	    print 'I excute 1 times'
 	    res *= base
 
         if is_minus:
